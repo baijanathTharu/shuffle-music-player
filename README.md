4ff6bb1e42d022e0eba6317ceb7b998b081e9789
baijanaththaru <bnchaudhary.tharu@gmail.com>
2020-07-29 22:08:43 +0545

    Initial Commit

--- README.md
+++ README.md
@@ -1,24 +1,55 @@
-# my-music
+# Shuffle Player
+
+This is music player which plays music in random order. You can see the demo [here](https://bntharu.com.np/projects/shuffle-player).
+
+---
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
-### Lints and fixes files
+##Instructions
+I have added three songs and you can try it without doing anything at all but if you want to play your songs then you must follow following steps.
+
+1. Rename each mp3 file that you want to play in number. For e.g. 0.mp3, 1.mp3, 2.mp3 and so on in increasing order.
+1. Place the mp3 files in the /src/assets/files directory.
+1. Place the wallpapers of each image in /public/pngs directory.
+
+1. In App.vue file, Change the value of the variable "numberOfSongs" to one greater than the number of songs you added on line 40.
+
 ```
-npm run lint
+  const numberOfSongs = NumberofSongsYouAdded + 1;
 ```
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+1. In App.vue file, edit the "songs" object according to the songs you have added. For e.g:
+
+```
+songs: [
+  {
+    id: 0,
+    title: "Title of your song",
+    artist: "Artist of the song",
+    wallpaper: "wallpaper-name.extension-name",
+  },
+  {
+    id: 1,
+    title: "Title of your song",
+    artist: "Artist of the song",
+    wallpaper: "wallpaper-name.extension-name",
+  },
+];
+```
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,274 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <h1>Shuffle Player</h1>
+    </header>
+    <main>
+      <section class="music-player">
+        <div class="wallpaper">
+          <img v-bind:src="'/pngs/' + this.currentSong[0].wallpaper" alt />
+        </div>
+        <div class="details">
+          <div class="song-title">
+            <h4 class="tracking-in-expand">{{this.currentSong[0].title}}</h4>
+          </div>
+          <div class="song-artist">
+            <h5 class="tracking-in-expand">{{this.currentSong[0].artist}}</h5>
+          </div>
+        </div>
+        <div class="controls">
+          <button
+            class="previous"
+            v-if="isPlayArrayLengthGreaterThanOne"
+            @click="prevAudio"
+          >Previous</button>
+          <button class="play" v-if="!isPlaying" @click="playAudio">Play</button>
+          <button class="pause" v-else @click="pauseAudio">Pause</button>
+          <button class="next" @click="nextAudio">Next</button>
+        </div>
+      </section>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      randNum() {
+        const numberOfSongs = 3;
+        return Math.floor(Math.random() * numberOfSongs);
+      },
+      isPlaying: false,
+      isPlayArrayLengthGreaterThanOne: false,
+      songs: [
+        {
+          id: 0,
+          title: "Isara",
+          artist: "Eluvetie",
+          wallpaper: "0.png",
+        },
+        {
+          id: 1,
+          title: "Music One",
+          artist: "Unknown",
+          wallpaper: "1.jpg",
+        },
+        {
+          id: 2,
+          title: "Music Two",
+          artist: "Unknown",
+          wallpaper: "2.jpg",
+        },
+        {
+          id: 3,
+          title: "Heart Like a Grave",
+          artist: "Insomnium",
+          wallpaper: "3.jpg",
+        },
+        {
+          id: 4,
+          title: "The Eagle Flies Alone",
+          artist: "Arch Enemy",
+          wallpaper: "4.jpg",
+        },
+        {
+          id: 5,
+          title: "Shadows of the Dying Sun",
+          artist: "Insomnium",
+          wallpaper: "5.jpg",
+        },
+        {
+          id: 6,
+          title: "Lose to Night",
+          artist: "Insomnium",
+          wallpaper: "6.jpg",
+        },
+        {
+          id: 7,
+          title: "While We Sleep",
+          artist: "Insomnium",
+          wallpaper: "7.jpg",
+        },
+        {
+          id: 8,
+          title: "The Promethean Song",
+          artist: "Insomnium",
+          wallpaper: "8.jpg",
+        },
+      ],
+      currentSong: [
+        {
+          id: 0,
+          title: "Isara",
+          artist: "Eluvetie",
+          wallpaper: "0.png",
+        },
+      ],
+      playArray: [
+        {
+          id: 0,
+          title: "Isara",
+          artist: "Eluvetie",
+          wallpaper: "0.png",
+        },
+      ],
+      audioObj: new Audio(require(`./assets/files/0.mp3`)),
+    };
+  },
+  created: function () {},
+  methods: {
+    playAudio() {
+      this.isPlaying = true;
+      this.audioObj.play();
+    },
+    pauseAudio() {
+      this.isPlaying = false;
+      this.audioObj.pause();
+    },
+    nextAudio() {
+      let randomSong = this.randNum();
+      while (randomSong === this.currentSong[0].id) {
+        randomSong = this.randNum();
+      }
+      this.audioObj.src = require(`./assets/files/${randomSong}.mp3`);
+      this.playArray.push(this.songs[randomSong]);
+      this.currentSong.splice(0, 1, this.songs[randomSong]);
+      this.isPlayArrayLengthGreaterThanOne = true;
+      this.audioObj.play();
+    },
+    prevAudio() {
+      const songId =
+        this.playArray[this.playArray.length - 2].id < 0
+          ? 0
+          : this.playArray[this.playArray.length - 2].id;
+      this.audioObj.src = require(`./assets/files/${songId}.mp3`);
+      this.isPlaying = true;
+      this.playArray.push(this.songs[songId]);
+      this.currentSong.splice(0, 1, this.songs[songId]);
+      this.audioObj.play();
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family: sans-serif;
+}
+header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgb(0, 0, 0);
+}
+
+header h1 {
+  color: white;
+  padding: 20px;
+  font-size: 24px;
+  font-weight: 400;
+}
+
+.music-player {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background-color: lightgrey;
+}
+
+.wallpaper {
+  /* background: teal; */
+  margin: 10px 0px;
+}
+
+.wallpaper img {
+  width: 250px;
+  height: 250px;
+  border: 1px solid black;
+  border-radius: 180px;
+}
+
+.details {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 20px;
+}
+
+.song-title h4 {
+  color: black;
+  padding: 10px;
+  font-size: 24px;
+}
+
+.tracking-in-expand {
+  -webkit-animation: tracking-in-expand 4s cubic-bezier(0.39, 0.575, 0.565, 1)
+    infinite both;
+  animation: tracking-in-expand 4s cubic-bezier(0.39, 0.575, 0.565, 1) infinite
+    both;
+}
+
+@-webkit-keyframes tracking-in-expand {
+  0% {
+    letter-spacing: -0.5em;
+    opacity: 0;
+  }
+  40% {
+    opacity: 0.6;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+@keyframes tracking-in-expand {
+  0% {
+    letter-spacing: -0.5em;
+    opacity: 0;
+  }
+  40% {
+    opacity: 0.6;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+.song-artist h5 {
+  color: black;
+  padding: 10px;
+  font-size: 16px;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  /* border: 1px solid black; */
+  padding: 10px;
+}
+
+.controls button {
+  margin: 10px;
+  padding: 10px;
+  font-size: 15px;
+  background-color: rgb(0, 0, 0, 0.85);
+  color: white;
+  outline: none;
+  border: none;
+  z-index: 5;
+}
+.controls button:hover {
+  cursor: pointer;
+  background-color: rgb(0, 0, 0);
 }
 </style>
